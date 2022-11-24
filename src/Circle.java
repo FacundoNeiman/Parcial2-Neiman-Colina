53d765b388f1978fce83d345548d78e14a15263a
Facundo Neiman y Kevin Colina <115724830+FacundoNeiman@users.noreply.github.com>
2022-11-24 11:36:13 -0300

    feat: add Circle class

---
+++ src/Circle.java
@@ -0,0 +1,40 @@
+public class Circle extends Shape {
+    public double radius;
+
+    public Circle(){
+        super();
+    }
+
+    public Circle(double radius){
+        super();
+        setRadius(radius);
+    }
+
+    public Circle(double radius, String color, boolean filled){
+        super(color, filled);
+        setRadius(radius);
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius,2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return Math.PI * (this.radius * 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[" + super.toString() + ", radius=" + this.radius + "]";
+    }
+}
